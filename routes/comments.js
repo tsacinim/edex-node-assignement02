7acef37af4b1beb723dcbdd8782fbb0551607763
tsacinim <33978445+tsacinim@users.noreply.github.com>
2017-11-26 15:42:17 +0100

    Add files via upload

---
+++ routes/comments.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+let store = require('../store');
+
+// routes to: /posts/:postId/comments
+module.exports = {
+  //gets comment data
+  // curl "http://localhost:3000/posts/:postId/comments"
+  getComments(req, res) {
+    const id = req.params.postId
+    // respond with the list of comments 
+    res.send(store.posts[id].comments)
+  }, 
+
+  //gets one comment by list index
+  // curl "http://localhost:3000/posts/0/comments/:commentId"
+  getOneComment(req, res) {
+    const postId = req.params.postId;
+    const commentId = req.params.commentId;
+    // send back the updated post 
+    res.send(store.posts[postId].comments[commentId]);
+  },
+
+  //posts comment data
+  // curl -H "Content-Type: application/json" -X POST -d '{"text": "boom!"}'  "http://localhost:3000/posts/:postId/comments"   
+  addComment(req, res) {
+    const id = req.params.postId;
+    if(!store.posts[id].comments) {
+      store.posts[id].comments = [];
+    }
+    // save changes in memory
+    store.posts[id].comments.push(req.body);
+    // persist changes to disk
+    fs.writeFileSync('./data.json', JSON.stringify(store,null,2));  
+    // send back id (index in list of comments) 
+    res.send({id: store.posts[id].comments.length - 1});
+  },
+  
+  //updates comment data at specific id
+  // curl -H 'Content-Type: application/json' -X PUT -d '{"text": "ping"}' "http://localhost:3000/posts/0/comments/3"
+  updateComment(req, res) {
+    const postId = req.params.postId;
+    const commentId = req.params.commentId;
+    // save changes in memory
+    store.posts[postId].comments[commentId] = req.body;
+    // persist changes to disk
+    fs.writeFileSync('./data.json', JSON.stringify(store,null,2));  
+    // send back the updated post 
+    res.send(store.posts[postId].comments[commentId]);
+  },
+  
+  //deletes comment data at specific id
+  // curl -X DELETE "http://localhost:3000/posts/0/comments/3" 
+  removeComment(req, res) {
+    const postId = req.params.postId;
+    const commentId = req.params.commentId;
+    // save changes in memory
+    store.posts[postId].comments[commentId] = {text: 'deleted'}
+    // persist changes to disk
+    fs.writeFileSync('./data.json', JSON.stringify(store,null,2));  
+    // send back the deleted post 
+    res.send(store.posts[postId].comments[commentId]); 
+  }  
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,60 @@
+// routes to: /posts
+const fs = require('fs');
+let store = require('../store');
+
+// The removal of a blog post must remove all its comments.
+module.exports = {
+  //gets all posts
+  // curl "http://localhost:3000/posts"
+  getPosts(req, res) {
+    res.send(store.posts);
+  },
+
+  //gets one post by list index
+  // curl "http://localhost:3000/posts/1"
+  getOnePost(req, res) {
+    const id = req.params.postId;
+    // send back the updated post 
+    res.send(store.posts[id]);
+  },
+
+  //posts post data
+  // curl -H "Content-Type: application/json" -X POST -d '{"name": "Top 10 ES6 Features", "url":"http://webapplog.com/es6", "text": ""}'  "http://localhost:3000/posts" 
+  addPost(req, res) {
+    // save changes in memory
+    store.posts.push(req.body);
+    // persist changes to disk
+    fs.writeFileSync('./data.json', JSON.stringify(store,null,2));  
+    // send back id (index in list of posts) 
+    res.send({id: store.posts.length - 1});
+  },
+
+  //updates post data at specific id
+  // curl -H 'Content-Type: application/json' -X PUT -d '{"name": "Top 10 ES6 Features Every Developer Must Know", "url":"http://webapplog.com/es6", "text": ""}' "http://localhost:3000/posts/0"
+  updatePost(req, res) {
+    const id = req.params.postId;
+    // save changes in memory
+    store.posts[id] = req.body;
+    // persist changes to disk
+    fs.writeFileSync('./data.json', JSON.stringify(store,null,2));  
+    // send back the updated post 
+    res.send(store.posts[id]);
+  },
+
+  //deletes post data at specific id
+  // curl -X DELETE "http://localhost:3000/posts/0" 
+  removePost(req, res) {
+    const id = req.params.postId;
+    // save changes in memory
+    store.posts[id] = {name: 'deleted'};
+    // persist changes to disk
+    fs.writeFileSync('./data.json', JSON.stringify(store,null,2));  
+    // send back the deleted post 
+    res.send(store.posts[id]);    
+  }
+}
+
+
+
+ 
 
